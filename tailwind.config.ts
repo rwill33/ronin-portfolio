332005006e21ca7a1ff397badd055cc6c5aa08c1
Ronin_Williams-Young <rwill33@uwo.ca>
2025-02-07 16:13:38 -0500

    Setup nextjs with aceternity and motion

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,53 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
+import flattenColorPalette from 'tailwindcss/lib/util/flattenColorPalette';
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        black: {
+          DEFAULT: '#000',
+          100: '#000319',
+        },
+      },
+      animation: {
+        aurora: 'aurora 60s linear infinite',
+      },
+      keyframes: {
+        aurora: {
+          from: {
+            backgroundPosition: '50% 50%, 50% 50%',
+          },
+          to: {
+            backgroundPosition: '350% 50%, 350% 50%',
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 } satisfies Config;
+
+// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
+
+interface VariablesForColorsAPI {
+  addBase: (styles: Record<string, string | Record<string, string>>) => void;
+  theme: <T>(path: string) => T;
+}
+
+function addVariablesForColors({ addBase, theme }: VariablesForColorsAPI) {
+  const allColors = flattenColorPalette(theme('colors'));
+  const newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, String(val)])
+  ) as Record<string, string>;
+
+  addBase({
+    ':root': newVars,
+  });
+}
