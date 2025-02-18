eafa0951c5a6c3af3eb52efd350781d218e96a8e
Ronin_Williams-Young <rwill33@uwo.ca>
2025-02-18 17:32:03 -0500

    Refactor top skills

--- components/Experience/TopSkills.tsx
+++ components/Experience/TopSkills.tsx
@@ -1,25 +1,25 @@
-import {
-  FaAws,
-  FaGitAlt,
-  FaJava,
-  FaJs,
-  FaNodeJs,
-  FaReact,
-} from 'react-icons/fa';
-import { PiFileSql } from 'react-icons/pi';
-import {
-  SiCss3,
-  SiDocker,
-  SiHtml5,
-  SiNestjs,
-  SiNextdotjs,
-  SiPostgresql,
-  SiPython,
-  SiTailwindcss,
-  SiTerraform,
-  SiTypescript,
-} from 'react-icons/si';
-import { VscAzure } from 'react-icons/vsc';
+import { skills } from '../skills';
+
+const topSkills = [
+  'react',
+  'typescript',
+  'nextjs',
+  'nestjs',
+  'nodejs',
+  'javascript',
+  'tailwindcss',
+  'aws',
+  'java',
+  'python',
+  'html5',
+  'css3',
+  'docker',
+  'terraform',
+  'sql',
+  'postgresSQL',
+  'git',
+  'azure',
+];
 
 const TopSkills = () => {
   return (
@@ -28,114 +28,17 @@ const TopSkills = () => {
         Top Skills
       </h4>
       <div className='grid grid-cols-3 gap-4 mt-4'>
-        <div className='flex flex-col items-center'>
-          <FaReact className='text-4xl text-blue-400' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            React
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiTypescript className='text-4xl text-blue-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            TypeScript
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiNextdotjs className='text-4xl text-black dark:text-white' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Next.js
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiNestjs className='text-4xl text-rose-600' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            NestJS
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <FaNodeJs className='text-4xl text-green-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Node.js
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <FaJs className='text-4xl text-yellow-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            JavaScript
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiTailwindcss className='text-4xl text-sky-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Tailwind CSS
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <FaAws className='text-4xl text-orange-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            AWS
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <FaJava className='text-4xl text-red-600' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Java
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiPython className='text-4xl text-blue-400' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Python
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiHtml5 className='text-4xl text-orange-600' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            HTML5
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiCss3 className='text-4xl text-blue-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            CSS3
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiDocker className='text-4xl text-blue-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Docker
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiTerraform className='text-4xl text-purple-800' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Terraform
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <PiFileSql className='text-4xl text-blue-600' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            SQL
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <SiPostgresql className='text-4xl text-blue-600' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            PostgresSQL
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <FaGitAlt className='text-4xl text-orange-600' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Git
-          </span>
-        </div>
-        <div className='flex flex-col items-center'>
-          <VscAzure className='text-4xl text-sky-500' />
-          <span className='mt-1 text-sm text-neutral-900 dark:text-neutral-100'>
-            Azure
-          </span>
-        </div>
+        {topSkills.map((key) => {
+          const skill = skills[key];
+          return (
+            <div key={skill.name} className='flex flex-col items-center'>
+              <skill.icon className={`text-4xl ${skill.color}`} />
+              <span className='mt-1 text-sm text-neutral-900 text-center dark:text-neutral-100'>
+                {skill.name}
+              </span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
---
+++ components/skills.ts
@@ -0,0 +1,122 @@
+import {
+  FaAws,
+  FaGitAlt,
+  FaJava,
+  FaJs,
+  FaNodeJs,
+  FaReact,
+} from 'react-icons/fa';
+import { IconType } from 'react-icons/lib';
+import { PiFileSql } from 'react-icons/pi';
+import {
+  SiCss3,
+  SiDocker,
+  SiHtml5,
+  SiNestjs,
+  SiNextdotjs,
+  SiPostgresql,
+  SiPython,
+  SiTailwindcss,
+  SiTerraform,
+  SiTypescript,
+} from 'react-icons/si';
+import { VscAzure } from 'react-icons/vsc';
+
+export interface Skill {
+  icon: IconType;
+  color: string;
+  name: string;
+}
+
+export const skills: Record<string, Skill> = {
+  react: {
+    icon: FaReact,
+    color: 'text-blue-400',
+    name: 'React',
+  },
+  typescript: {
+    icon: SiTypescript,
+    color: 'text-blue-500',
+    name: 'TypeScript',
+  },
+  nextjs: {
+    icon: SiNextdotjs,
+    color: 'text-black dark:text-white',
+    name: 'Next.js',
+  },
+  nestjs: {
+    icon: SiNestjs,
+    color: 'text-rose-600',
+    name: 'NestJS',
+  },
+  nodejs: {
+    icon: FaNodeJs,
+    color: 'text-green-500',
+    name: 'Node.js',
+  },
+  javascript: {
+    icon: FaJs,
+    color: 'text-yellow-500',
+    name: 'JavaScript',
+  },
+  tailwindcss: {
+    icon: SiTailwindcss,
+    color: 'text-sky-500',
+    name: 'Tailwind CSS',
+  },
+  aws: {
+    icon: FaAws,
+    color: 'text-orange-500',
+    name: 'AWS',
+  },
+  java: {
+    icon: FaJava,
+    color: 'text-red-600',
+    name: 'Java',
+  },
+  python: {
+    icon: SiPython,
+    color: 'text-blue-400',
+    name: 'Python',
+  },
+  html5: {
+    icon: SiHtml5,
+    color: 'text-orange-600',
+    name: 'HTML5',
+  },
+  css3: {
+    icon: SiCss3,
+    color: 'text-blue-500',
+    name: 'CSS3',
+  },
+  docker: {
+    icon: SiDocker,
+    color: 'text-blue-500',
+    name: 'Docker',
+  },
+  terraform: {
+    icon: SiTerraform,
+    color: 'text-purple-800',
+    name: 'Terraform',
+  },
+  sql: {
+    icon: PiFileSql,
+    color: 'text-blue-600',
+    name: 'SQL',
+  },
+  postgresql: {
+    icon: SiPostgresql,
+    color: 'text-blue-600',
+    name: 'PostgresSQL',
+  },
+  git: {
+    icon: FaGitAlt,
+    color: 'text-orange-600',
+    name: 'Git',
+  },
+  azure: {
+    icon: VscAzure,
+    color: 'text-sky-500',
+    name: 'Azure',
+  },
+};
