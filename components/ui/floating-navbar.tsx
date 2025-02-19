0fc2d26eed29f3de3326c158d937bc52872df6f6
Ronin_Williams-Young <rwill33@uwo.ca>
2025-02-18 23:09:04 -0500

    Change contact button to link

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -36,10 +36,13 @@ export const FloatingNav = ({
           </Link>
         )
       )}
-      <button className='border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full'>
+      <a
+        href='mailto:ronin@williams-young.com'
+        className='border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 rounded-full'
+      >
         <span>Contact</span>
         <span className='absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent  h-px' />
-      </button>
+      </a>
     </div>
   );
 };
