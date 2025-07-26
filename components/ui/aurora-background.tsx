6b18b819d40658840550ee340a46266ef70c5dda
Ronin_Williams-Young <rwill33@uwo.ca>
2025-07-25 21:55:55 -0400

    Edit background for dark mode

--- components/ui/aurora-background.tsx
+++ components/ui/aurora-background.tsx
@@ -16,7 +16,7 @@ export const AuroraBackground = ({
   return (
     <div
       className={cn(
-        'relative flex flex-col items-center bg-gradient-to-br from-blue-400 via-purple-500 to-purple-600 dark:bg-zinc-900 text-white dark:text-slate-950 transition-all duration-500',
+        'relative flex flex-col items-center bg-gradient-to-br from-blue-400 via-purple-500 to-purple-600 dark:bg-gradient-to-br dark:from-purple-900 dark:via-violet-900 dark:to-indigo-900 text-white dark:text-white transition-all duration-500',
         className
       )}
       {...props}
@@ -27,14 +27,15 @@ export const AuroraBackground = ({
             `
             [--white-gradient:repeating-linear-gradient(100deg,var(--white)_0%,var(--white)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--white)_16%)]
             [--dark-gradient:repeating-linear-gradient(100deg,var(--black)_0%,var(--black)_7%,var(--transparent)_10%,var(--transparent)_12%,var(--black)_16%)]
+            [--dark-aurora:repeating-linear-gradient(100deg,var(--blue-900)_10%,var(--blue-800)_15%,var(--purple-800)_20%,var(--violet-800)_25%,var(--indigo-800)_30%)]
             [--aurora:repeating-linear-gradient(100deg,var(--blue-500)_10%,var(--indigo-300)_15%,var(--blue-300)_20%,var(--violet-200)_25%,var(--blue-400)_30%)]
             [background-image:var(--white-gradient),var(--aurora)]
-            dark:[background-image:var(--dark-gradient),var(--aurora)]
+            dark:[background-image:var(--aurora)]
             [background-size:300%,_200%]
             [background-position:50%_50%,50%_50%]
             filter blur-[10px] invert-0 dark:invert
             after:content-[""] after:absolute after:inset-0 after:[background-image:var(--white-gradient),var(--aurora)] 
-            after:dark:[background-image:var(--dark-gradient),var(--aurora)]
+            after:dark:[background-image:var(--aurora)]
             after:[background-size:200%,_100%] 
             after:animate-aurora after:[background-attachment:fixed] after:mix-blend-difference
             pointer-events-none
