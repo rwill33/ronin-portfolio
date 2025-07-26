833200d918ab500bc09bb06d4d905326942ac67b
Ronin_Williams-Young <rwill33@uwo.ca>
2025-07-25 22:01:43 -0400

    Match dark mode liquid glass navbar

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -20,7 +20,7 @@ export const FloatingNav = ({
   };
 
   return (
-    <div className='flex max-w-fit inset-x-0 my-6 mx-auto border border-white/20 dark:border-white/[0.2] rounded-full bg-white/20 backdrop-blur-md dark:bg-zinc-900 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-8 py-4  items-center justify-center space-x-4'>
+    <div className='flex max-w-fit inset-x-0 my-6 mx-auto border border-white/20 dark:border-white/10 rounded-full bg-white/20 backdrop-blur-md dark:bg-black/20 dark:backdrop-blur-sm shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] dark:shadow-2xl z-[5000] px-8 py-4  items-center justify-center space-x-4'>
       {navItems.map(
         (
           navItem: {
